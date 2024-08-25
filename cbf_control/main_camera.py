bf85a20d3e4a9697e596e6340634edd16f4fed86
fer <fernandorecalde@uti.edu.ec>
2024-08-24 20:55:21 -0500

    Updates

--- cbf_control/main_camera.py
+++ cbf_control/main_camera.py
@@ -10,6 +10,7 @@ from cbf_control import forward_kinematics_casadi_camera
 from cbf_control import casadi_camera
 from cbf_control import dualquat_trans_casadi, dualquat_quat_casadi, rotation_casadi, rotation_inverse_casadi, dual_velocity_casadi, dual_quat_casadi, velocities_from_twist_casadi
 from cbf_control import create_ocp_solver
+from scipy.linalg import block_diag
 
 import threading
 import time
@@ -64,8 +65,9 @@ class CameraNode(Node):
         self.N_prediction = self.N.shape[0]
 
         # Initial States dual set zeros
-        pos_0 = np.array([0.0, 0.0, 0.0], dtype=np.double)
-        theta_0 = 0.0
+        pos_0 = np.array([0.0, -1, 0.0], dtype=np.double)
+        #pos_0 = np.array([0.0, -0.0, 0.0], dtype=np.double)
+        theta_0 = np.pi/4
         n_0 = np.array([0.0, 0.0, 1.0])
         quat_0 = np.hstack([np.cos(theta_0 / 2), np.sin(theta_0 / 2) * np.array(n_0)])
         #quat_0 = np.array([1.0, 0.0, 0.0, 0.0])
@@ -85,7 +87,8 @@ class CameraNode(Node):
         self.body_msg = Odometry()
         self.publisher_body_ = self.create_publisher(Odometry, "body", 10)
 
-        self.publisher_point_ = self.create_publisher(PointStamped, 'point', 10)
+        self.publisher_point_ = self.create_publisher(PointStamped, 'point_1', 10)
+        self.publisher_point_1_ = self.create_publisher(PointStamped, 'point_2', 10)
 
         # fames
         self.frames = TransformBroadcaster(self)
@@ -103,12 +106,23 @@ class CameraNode(Node):
         self.point_in_w = PointStamped()
         self.point_in_w.header.frame_id = 'world'
         self.point_in_w.header.stamp = self.get_clock().now().to_msg()
-        self.point_in_w.point.x = 1.0
+        self.point_in_w.point.x = 1.2
         self.point_in_w.point.y = 0.0
-        self.point_in_w.point.z = 0.0
+        self.point_in_w.point.z = 0.1
+
+        # Define a point in the W frame
+        self.point_in_w1 = PointStamped()
+        self.point_in_w1.header.frame_id = 'world'
+        self.point_in_w1.header.stamp = self.get_clock().now().to_msg()
+        self.point_in_w1.point.x = 1.0
+        self.point_in_w1.point.y = 0.2
+        self.point_in_w1.point.z = 0.0
 
         self.x_w = np.zeros((3, self.t.shape[0] + 1 - self.N_prediction), dtype=np.double)
         self.x_w[:, 0] =  np.array([self.point_in_w.point.x, self.point_in_w.point.y, self.point_in_w.point.z])
+
+        self.x_w1 = np.zeros((3, self.t.shape[0] + 1 - self.N_prediction), dtype=np.double)
+        self.x_w1[:, 0] =  np.array([self.point_in_w1.point.x, self.point_in_w1.point.y, self.point_in_w1.point.z])
 
         # Numerical Intgeration of the system
         ocp = create_ocp_solver(self.X[:, 0], self.N_prediction, self.t_N, self.L, self.ts)
@@ -226,19 +240,21 @@ class CameraNode(Node):
         
         # Point in the world frame 
         x_w = np.array([self.point_in_w.point.x,  self.point_in_w.point.y, self.point_in_w.point.z])
+        x_w1 = np.array([self.point_in_w1.point.x,  self.point_in_w1.point.y, self.point_in_w1.point.z])
 
-        vector = inverse_rot(q, x_w - position_b) - inverse_rot(q_c, position_cb)
-        return np.array(vector).reshape((3, ))
+        vector1 = inverse_rot(q, x_w - position_b) - inverse_rot(q_c, position_cb)
+        vector2 = inverse_rot(q, x_w1 - position_b) - inverse_rot(q_c, position_cb)
+        return np.array(vector1).reshape((3, )), np.array(vector2).reshape((3, ))
         
-    def update_point(self, x):
+    def update_point(self, x1, point_in_w, publisher_point_):
         # Define a point in the W frame
-        self.point_in_w.header.frame_id = 'world'
-        self.point_in_w.header.stamp = self.get_clock().now().to_msg()
-        self.point_in_w.point.x = x[0]
-        self.point_in_w.point.y = x[1]
-        self.point_in_w.point.z = x[2]
+        point_in_w.header.frame_id = 'world'
+        point_in_w.header.stamp = self.get_clock().now().to_msg()
+        point_in_w.point.x = x1[0]
+        point_in_w.point.y = x1[1]
+        point_in_w.point.z = x1[2]
         # Publish the message
-        self.publisher_point_.publish(self.point_in_w)
+        publisher_point_.publish(point_in_w)
         return None
 
     def H_plus(self, q):
@@ -301,9 +317,11 @@ class CameraNode(Node):
         
         # Point in the world frame 
         x_w = np.array([0.0, self.point_in_w.point.x,  self.point_in_w.point.y, self.point_in_w.point.z])
+        x_w1 = np.array([0.0, self.point_in_w1.point.x,  self.point_in_w1.point.y, self.point_in_w1.point.z])
 
-        x_c = H_plus_c.T@H_minus_c@(x_w - position_b) - H_plus_q_bc.T@H_minus_q_bc@position_bc
-        return x_c[1:4]
+        x_c1 = H_plus_c.T@H_minus_c@(x_w - position_b) - H_plus_q_bc.T@H_minus_q_bc@position_bc
+        x_c2 = H_plus_c.T@H_minus_c@(x_w1 - position_b) - H_plus_q_bc.T@H_minus_q_bc@position_bc
+        return x_c1[1:4], x_c2[1:4]
 
     def l2_norm(self, x_cd, x_max, y_max):
         # Desired Point camera frame
@@ -386,16 +404,13 @@ class CameraNode(Node):
         Jaux = np.array([[a, b]])
         J = aux*Jaux
         return  J
-
-    def control_law_camera_frame(self, X_b, X_c, X, x_c, x_cd, x_dot_w):
-        # Compute quaternion and translations of the final frame
+    def Jacobian_camera_frame(self, X_b, X_c, X, x_c):
         c = np.array(get_quat(X)).reshape((4, ))
         H_plus_c = self.H_plus(c)
         H_minus_c = self.H_minus(c)
 
         # Compute orientation and translation body frame respect to the world frame
         t_b = np.array(get_trans(X_b)).reshape((4, ))
-        position_b = np.array([0.0, t_b[1], t_b[2], t_b[3]]).reshape((4, ))
 
         # Compute quaternion and translations of the camera respect to body
         t_bc = np.array(get_trans(X_c)).reshape((4, ))
@@ -413,11 +428,9 @@ class CameraNode(Node):
         a = H_plus_q_bc@position_c + H_minus_q_bc@position_bc
         b = H_minus_q_bc.T@position_c + H_plus_q_bc.T@position_bc
 
-        H_plus_a = self.H_plus(a)
         H_minus_a = self.H_minus(a)
 
         H_plus_b = self.H_plus(b)
-        H_minus_b = self.H_minus(b)
 
         # Compute Jacobians
         T = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
@@ -431,9 +444,24 @@ class CameraNode(Node):
 
         # Reduce Dimensions
         J = T2@J
+        return J, J3
+        
+    def control_law_camera_frame(self, X_b, X_c, X, x_c_1, x_cd_1, x_c1_dot_w, x_c_2, x_cd_2, x_c2_dot_w):
+        J1, J31 = self.Jacobian_camera_frame(X_b, X_c, X, x_c_1)
+        J2, J32 = self.Jacobian_camera_frame(X_b, X_c, X, x_c_2)
+        J = np.vstack((J1, J2))
+        J3 = block_diag(J31, J32)
+
+        x_c_dot_w = np.vstack((x_c1_dot_w.reshape((3, 1)), x_c2_dot_w.reshape((3, 1))))
 
         # Error Control
-        x_error = x_cd - x_c
+        x_error1 = x_cd_1 - x_c_1
+        x_error2 = x_cd_2 - x_c_2
+
+        x_error1 = x_error1.reshape((3, 1))
+        x_error2 = x_error2.reshape((3, 1))
+
+        x_error = np.vstack((x_error1, x_error2))
 
         # Null space projection
         #W = np.diag([1, 1, 1, 1, 1, 1])
@@ -441,19 +469,19 @@ class CameraNode(Node):
         I = np.diag([1, 1, 1, 1, 1, 1])
 
         # Desired Velocities Null space
-        v_d_i = np.array([0.0, 0.0, 0.0])
-        w_d_b = np.array([0.0, 0.0, 0.0])
+        v_d_i = np.array([0.0, 0.0, 0.0]).reshape((3, 1))
+        w_d_b = np.array([0.0, 0.0, 0.0]).reshape((3, 1))
         # General Vector velocities
-        v_d = np.hstack((v_d_i, w_d_b))
+        v_d = np.vstack((w_d_b, v_d_i))
 
         J_inverse = np.linalg.inv(W)@J.T@np.linalg.inv(J@np.linalg.inv(W)@J.T)
-        K = 1*np.diag([1, 1, 1])
+        K = 0.1*np.diag([1, 1, 1, 1, 1, 1])
 
         # Control Law
-        u = J_inverse@(K@x_error - J3@x_dot_w) + (I + J_inverse@J)@v_d
         #u = J_inverse@(K@x_error) + (I + J_inverse@J)@v_d
-        #u = np.linalg.pinv(J)@(x_error)
-        return u
+        #u = J_inverse@(K@x_error) + (I + J_inverse@J)@v_d
+        u = np.linalg.pinv(J)@(0.1*x_error) + (I + J_inverse@J)@v_d
+        return u[:, 0]
 
     def control_law_camera_frame_exp(self, X_b, X_c, X, x_c, x_cd, x_dot_w, n, kx, ky, kz, r_max_x, r_max_y, r_max_z):
         # Compute quaternion and translations of the final frame
@@ -463,7 +491,6 @@ class CameraNode(Node):
 
         # Compute orientation and translation body frame respect to the world frame
         t_b = np.array(get_trans(X_b)).reshape((4, ))
-        position_b = np.array([0.0, t_b[1], t_b[2], t_b[3]]).reshape((4, ))
 
         # Compute quaternion and translations of the camera respect to body
         t_bc = np.array(get_trans(X_c)).reshape((4, ))
@@ -481,11 +508,9 @@ class CameraNode(Node):
         a = H_plus_q_bc@position_c + H_minus_q_bc@position_bc
         b = H_minus_q_bc.T@position_c + H_plus_q_bc.T@position_bc
 
-        H_plus_a = self.H_plus(a)
         H_minus_a = self.H_minus(a)
 
         H_plus_b = self.H_plus(b)
-        H_minus_b = self.H_minus(b)
 
         # Compute Jacobians
         T = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
@@ -511,7 +536,7 @@ class CameraNode(Node):
 
         # Null space projection
         W = np.diag([1, 1, 1, 1, 1, 1])
-        W = np.diag([300, 300, 1, 1, 1, 1])
+        W = np.diag([500, 500, 1, 1, 1, 500])
         I = np.diag([1, 1, 1, 1, 1, 1])
 
         # Desired Velocities Null space
@@ -572,9 +597,13 @@ class CameraNode(Node):
 
         # Velocity Object
         v_w = np.zeros((3, self.t.shape[0]), dtype=np.double)
-        v_w[0, :] = 0.0*np.sin(self.t)
-        v_w[1, :] = 0.1*np.cos(self.t)
+        #v_w[0, :] = 0.0*np.sin(self.t)
+        #v_w[1, :] = 0.1*np.cos(self.t)
         #v_w[2, :] = 0*np.cos(self.t)
+
+        v_w1 = np.zeros((3, self.t.shape[0]), dtype=np.double)
+        #v_w1[0, :] = 0.0*np.sin(self.t)
+        #v_w1[1, :] = 0.1*np.cos(self.t)
 
         # Empty vector current values b frame
         x_b_data = np.zeros((3, self.t.shape[0] - self.N_prediction), dtype=np.double)
@@ -583,13 +612,18 @@ class CameraNode(Node):
         # Desired Point B frame
         x_bd = np.zeros((3, self.t.shape[0] - self.N_prediction), dtype=np.double)
         x_bd[0, :] = 0.0
-        x_bd[1, :] = 0.0
+        x_bd[1, :] = -0.1
         x_bd[2, :] = -0.2
 
         x_cd = np.zeros((3, self.t.shape[0] - self.N_prediction), dtype=np.double)
         x_cd[0, :] = 0.0
-        x_cd[1, :] = 0.0
-        x_cd[2, :] = 0.5
+        x_cd[1, :] = -0.05
+        x_cd[2, :] = 0.9
+        
+        x_cd1 = np.zeros((3, self.t.shape[0] - self.N_prediction), dtype=np.double)
+        x_cd1[0, :] = -0.2
+        x_cd1[1, :] = 0.05
+        x_cd1[2, :] = 0.7
 
         # Map L2 Norm
         x_max = 1.5
@@ -609,34 +643,39 @@ class CameraNode(Node):
             # Get model
             tic = time.time()
             # Update point for the projection
-            self.update_point(self.x_w[:, k])
+            self.update_point(self.x_w[:, k], self.point_in_w, self.publisher_point_)
+            self.update_point(self.x_w1[:, k], self.point_in_w1, self.publisher_point_1_)
             # Point Projected to the body frame using tf in the background and manual computation
             x_b = self.transform_point('body')
             x_b_aux = self.transform_point_manual(self.X[:, k])
             x_b_aux_2 = self.transform_point_manual_matrix_form(self.X[:, k])
-            x_b_data[:, k] = x_b_aux
-
-            # Control Law body frame
-            #self.u[:, k] = self.control_law(x_bd[:, k], x_b_aux, self.X[:, k], v_w[:, k])
 
             # Camera orientation Dualquaternion
             self.forward_camera = forward_camera_f(self.X[0:4, k], self.X[4:8, k], 0.0)
 
             # Point Projected to the camera frame using tf and manual computation
             x_c = self.transform_point("camera")
-            x_c_aux = self.transform_point_manual_camera(self.X[:, k], self.camera_pose, self.forward_camera)
-            x_c_aux_2 = self.transform_point_manual_camera_matrix_form(self.X[:, k], self.camera_pose, self.forward_camera)
+            x_c_aux, x_c_2_aux = self.transform_point_manual_camera(self.X[:, k], self.camera_pose, self.forward_camera)
+            x_c_aux_2, x_c_2_aux_2 = self.transform_point_manual_camera_matrix_form(self.X[:, k], self.camera_pose, self.forward_camera)
             x_c_data[:, k] = x_c_aux
 
             # Print values
-            #print(x_c_aux)
+            print(x_c_aux)
+            print(x_c_2_aux)
+            print("--------")
+            print(x_c_aux_2)
+            print(x_c_2_aux_2)
             t_camera_body = get_trans(self.camera_pose)
             q_camera_body = get_quat(self.camera_pose)
-            print(t_camera_body)
-            print(q_camera_body)
+            #print(t_camera_body)
+            #print(q_camera_body)
+
 
             # Control Lawcamera frame
-            self.u[:, k] = self.control_law_camera_frame(self.X[:, k], self.camera_pose, self.forward_camera, x_c_aux, x_cd[:, k], v_w[:, k])
+            self.u[:, k] = self.control_law_camera_frame(self.X[:, k], self.camera_pose, self.forward_camera, x_c_aux_2, x_cd[:, k], v_w[:, k], x_c_2_aux_2, x_cd1[:, k], v_w1[:, k])
+            self.u[0, k] = 0.0
+            self.u[1, k] = 0.0
+            self.u[5, k] = 0.0
             #self.u[:, k] = self.control_law_camera_frame_exp(self.X[:, k], self.camera_pose, self.forward_camera, x_c_aux, x_cd[:, k], v_w[:, k], n, kx, ky, kz, r_max_x, r_max_y, r_max_z)
 
             # Publish TF information body, camera and usign dualquaternion
@@ -653,6 +692,7 @@ class CameraNode(Node):
             self.X[:, k + 1] = xcurrent
 
             # Update point information
+            self.x_w1[:, k + 1] = self.f_d(self.x_w1[:, k], v_w1[:, k])
             self.x_w[:, k + 1] = self.f_d(self.x_w[:, k], v_w[:, k])
             # Section to guarantee same sample times
             while (time.time() - tic <= self.ts):
@@ -660,18 +700,16 @@ class CameraNode(Node):
             toc = time.time() - tic
             self.get_logger().info("Camera Projections")
         
-        #fig11, ax11, ax12, ax13 = fancy_plots_3()
-        #plot_states_position_b_frame(fig11, ax11, ax12, ax13, x_c_data[0:3, :], x_cd[0:3, :], self.t, "Position Results Body Frame Point "+ str(self.initial), self.path_image_file)
-        #plt.show()
-
-        #fig21, ax21, ax22, ax23 = fancy_plots_3()
-        #plot_linear_velocities(fig21, ax21, ax22, ax23, self.u[3:6, :], self.t, "Linear Velocity Inertial Frame "+ str(self.initial), self.path_image_file)
-        #plt.show()
-        #
-        #fig31, ax31, ax32, ax33 = fancy_plots_3()
-        #plot_angular_velocities(fig31, ax31, ax32, ax33, self.u[0:3, :], self.t, "Angular Velocity Body Frame "+ str(self.initial), self.path_image_file)
-        #plt.show()
-
+        fig11, ax11, ax12, ax13 = fancy_plots_3()
+        plot_states_position_b_frame(fig11, ax11, ax12, ax13, x_c_data[0:3, :], x_cd[0:3, :], self.t, "Position Results Body Frame Point "+ str(self.initial), self.path_image_file)
+        plt.show()
+        fig21, ax21, ax22, ax23 = fancy_plots_3()
+        plot_linear_velocities(fig21, ax21, ax22, ax23, self.u[3:6, :], self.t, "Linear Velocity Inertial Frame "+ str(self.initial), self.path_image_file)
+        plt.show()
+        
+        fig31, ax31, ax32, ax33 = fancy_plots_3()
+        plot_angular_velocities(fig31, ax31, ax32, ax33, self.u[0:3, :], self.t, "Angular Velocity Body Frame "+ str(self.initial), self.path_image_file)
+        plt.show()
         #plt.figure(figsize=(12, 6))
         #plt.subplot(1, 2, 1)
         #plt.imshow(Z_smooth_norm, extent=[-x_max, x_max, -y_max, y_max], origin='lower', cmap='viridis')
